64ed8a1e9d07125c427bcd3a09b4bac3cd33d0c2
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-08-15 23:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (41.66%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int maxDepth(string s) {
-        int count = 0, ans = INT_MIN;;
-	    for(char ch: s){
-		    if(ch == '(') count++;
-		    else if(ch == ')') count--;
-		    ans = max(ans, count);
-	    }
-	    return ans;
-    }
+		int maxDepth(string s) {
+			int count = 0, ans = INT_MIN;;
+			for(char ch: s){
+				if(ch == '(') count++;
+				else if(ch == ')') count--;
+				ans = max(ans, count);
+			}
+			return ans;
+		}
 };
