4b44537c5fee7c265c0619d15d93820cf03fd4f5
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-12-19 22:51:18 +0530

    Time: 55 ms (89.30%) | Memory: 13.7 MB (29.23%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> matrix(n, vector<int>(n, INT_MAX));
+        vector<pair<int, int>> adj[n];
         for(auto i: edges) {
-            matrix[i[0]][i[1]] = i[2];
-            matrix[i[1]][i[0]] = i[2];
+            adj[i[0]].push_back({i[1], i[2]});
+            adj[i[1]].push_back({i[0], i[2]});
         }
-        
-        for(int i = 0; i < n; i++) matrix[i][i] = 0;
-        
-        for(int via = 0; via < n; via++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(matrix[i][via] == INT_MAX || matrix[via][j] == INT_MAX) continue;
-                    
-                    matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
-                }
-            }
-        }
-        
-        int countCity = n;
-        int city = -1;
+
+        int countCity = n, city = -1;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
 
         for(int i = 0; i < n; i++) {
+            q.push({0, i});
+            vector<int> dist(n, 1e9);
+            dist[i] = 0;
+
+            while(!q.empty()) {
+                int distance = q.top().first;
+                int node = q.top().second;
+                q.pop();
+
+                for(auto it: adj[node]) {
+                    int weight = it.second;
+                    int adjacentNode = it.first;
+                    if(distance + weight < dist[adjacentNode]) {
+                        dist[adjacentNode] = distance + weight;
+                        q.push({distance + weight, adjacentNode});
+                    }
+                }
+            }
+
             int count = 0;
             for(int j = 0; j < n; j++) {
-                if(matrix[i][j] <= distanceThreshold) count++;
+                if(dist[j] <= distanceThreshold) count++;
             }
-            
             if(count <= countCity) {
                 countCity = count;
                 city = i;
