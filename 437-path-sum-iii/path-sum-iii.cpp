db7fc07df7d7a6d6e6a09ef61f839ad3e984fa24
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-12-21 22:10:51 +0530

    Time: 27 ms (24.32%) | Memory: 16.5 MB (57.60%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, long int targetSum) {
+        if(!root) return 0;
+        
+        int count = 0;
+        if(root->val == targetSum) count++;
+        count += dfs(root->left, targetSum - root->val);
+        count += dfs(root->right, targetSum - root->val);
+
+        return count;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if(!root) return 0;
+
+        int count = dfs(root, targetSum);
+        count += pathSum(root->left, targetSum);
+        count += pathSum(root->right, targetSum);
+
+        return count;
+    }
+};
